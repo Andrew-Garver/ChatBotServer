526365025383553d3fec0a2ae4e2d8e494d0c2b8
Andrew Garver <andrewsgarver@gmail.com>
2017-12-13 14:33:45 -0500

    Added production libraries and node .gitignore

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const cors           = require('cors');
+const compression    = require('compression');
+const helmet         = require('helmet');
 const app            = express();
 
 const port = 8000;
 
+app.use(compression());
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 
